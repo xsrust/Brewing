98b9faf3249d3e4133384ce0f722e0e5cd880678
Sam Rust <xsrust@gmail.com>
2019-08-14 00:03:00 +0100

    fermentables with enums

--- app/models/fermentable.rb
+++ app/models/fermentable.rb
@@ -14,8 +14,13 @@
 #
 
 class Fermentable < ApplicationRecord
+  enum type: [:dry_extract, :extract, :grain, :sugar, :fruit, :juice, :honey,
+    :other]
+  enum grain_group: [ :base, :caramel, :flaked, :roasted, :specialty, :smoked,
+    :adjunct ]
   has_many :fermentable_additions
   has_many :recipes, through: :fermentable_additions
+
 
   #disable inheritance column
   self.inheritance_column = :_type_disabled
